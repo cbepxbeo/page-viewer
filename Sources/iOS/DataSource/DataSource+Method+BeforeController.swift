f3b14ceba517301c5846634ef99fbf5c6127f944
cbepxbeo <cbepxbeo@mail.ru>
2023-08-26 02:06:06 +0300

    Create DataSource

---
+++ Sources/iOS/DataSource/DataSource+Method+BeforeController.swift
@@ -0,0 +1,33 @@
+/*
+ 
+ Project: PageViewer
+ File: DataSource+Method+BeforeController.swift
+ Created by: Egor Boyko
+ Date: 24.08.2023
+ 
+ Status: #Complete | #Not decorated
+ 
+ */
+
+import SwiftUI
+
+extension DataSource {
+    @inlinable
+    func beforeController(
+        _ pageViewController: UIPageViewController,
+        _ viewController: UIViewController) -> UIViewController? {
+            guard
+                let hosting = viewController as? Hosting<Content>
+            else {
+                return nil
+            }
+            
+            if !self.looped && hosting.index == 0 {
+                return nil
+            }
+            
+            let index = hosting.index == 0 ? self.total - 1 : hosting.index - 1
+            self.lastIndex = index
+            return self.views[index]
+        }
+}
---
+++ Sources/iOS/DataSource/DataSource.swift
@@ -0,0 +1,58 @@
+/*
+ 
+ Project: PageViewer
+ File: DataSource.swift
+ Created by: Egor Boyko
+ Date: 24.08.2023
+ 
+ Status: #Complete | #Not decorated
+ 
+ */
+
+import SwiftUI
+
+final class DataSource<Content: View>: NSObject, UIPageViewControllerDataSource {
+    init(views: [Content], lastIndex: Int, looped: Bool) {
+        self.coordinator = nil
+        self.lastIndex = lastIndex
+        self.looped = looped
+        var temp: [Hosting<Content>] = []
+        for (index, element) in views.enumerated() {
+            temp.append(Hosting(index: index, rootView: element))
+        }
+        self.views = temp
+        self.total = temp.count
+    }
+    var looped: Bool
+    var total: Int
+    var lastIndex: Int
+    let views: [Hosting<Content>]
+    lazy var root: Hosting<Content>? = {
+        if total == 0 { return nil }
+        
+        if lastIndex <= 0 {
+            return self.views.first
+        }
+        
+        if lastIndex >= total {
+            return self.views.last
+        }
+
+        return self.views[lastIndex]
+    }()
+    
+    func pageViewController(
+        _ pageViewController: UIPageViewController,
+        viewControllerBefore viewController: UIViewController) -> UIViewController? {
+            self.beforeController(pageViewController, viewController)
+        }
+    
+    func pageViewController(
+        _ pageViewController: UIPageViewController,
+        viewControllerAfter viewController: UIViewController) -> UIViewController? {
+            self.afterController(pageViewController, viewController)
+        }
+    
+    weak var coordinator: Coordinator<Content>?
+    
+}
