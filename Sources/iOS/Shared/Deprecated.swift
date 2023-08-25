81bcc3cba741eb0eb4188a9d7a651d0a44a4227c
cbepxbeo <cbepxbeo@mail.ru>
2023-08-26 02:09:04 +0300

    Create Deprecated.swift

---
+++ Sources/iOS/Shared/Deprecated.swift
@@ -0,0 +1,13 @@
+/*
+ 
+ Project: PageViewer
+ File: Deprecated.swift
+ Created by: Egor Boyko
+ Date: 24.08.2023
+ 
+ Status: #Completed | #Not required
+ 
+ */
+
+@available(*,deprecated, renamed: "PageView", message: "Will not be available in the next version")
+typealias PageViewerView = PageView
