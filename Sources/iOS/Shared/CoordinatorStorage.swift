529585099929e83d5693da36c596d18306872a1f
cbepxbeo <cbepxbeo@mail.ru>
2023-08-26 02:07:37 +0300

    Create CoordinatorStorage.swift

---
+++ Sources/iOS/Shared/CoordinatorStorage.swift
@@ -0,0 +1,14 @@
+/*
+ 
+ Project: PageViewer
+ File: CoordinatorStorage.swift
+ Created by: Egor Boyko
+ Date: 26.08.2023
+ 
+ Status: #Complete | #Not decorated
+ 
+ */
+
+public struct CoordinatorStorage {
+    weak var coordinator: PageViewCoordinator?
+}
