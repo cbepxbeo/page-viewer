1e79fb7466548e38e460feda92e1a7a5a8f906e7
cbepxbeo <cbepxbeo@mail.ru>
2023-08-26 02:07:32 +0300

    Create PageViewDelegate.swift

---
+++ Sources/iOS/Protocols/PageViewDelegate/PageViewDelegate.swift
@@ -0,0 +1,15 @@
+/*
+ 
+ Project: PageViewer
+ File: PageViewDelegate.swift
+ Created by: Egor Boyko
+ Date: 25.08.2023
+ 
+ Status: #Completed | #Not required
+ 
+ */
+
+///Use a protocol if you want to receive calls about events in the PageView
+public protocol PageViewDelegate: AnyObject {
+    var total: Int { get set }
+}
