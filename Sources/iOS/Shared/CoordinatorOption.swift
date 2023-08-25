7e3cd0d59238aed0eb90316e45559ac7de513275
cbepxbeo <cbepxbeo@mail.ru>
2023-08-26 02:07:35 +0300

    Create CoordinatorOption.swift

---
+++ Sources/iOS/Shared/CoordinatorOption.swift
@@ -0,0 +1,15 @@
+/*
+ 
+ Project: PageViewer
+ File: CoordinatorOption.swift
+ Created by: Egor Boyko
+ Date: 26.08.2023
+ 
+ Status: #Complete | #Not decorated
+ 
+ */
+
+public enum CoordinatorOption {
+    case first
+    case last
+}
